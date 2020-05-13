438a6aa79d7412b8e15eadbdd8db4d62a8643d86
Jineshfrancisco <jineshfrancis@hotmail.com>
2020-05-14 02:40:50 +0530

    home page updated

--- application/views/home.php
+++ application/views/home.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo base_url(); ?>public/css/style.css">
-    <title>Welcome to Codeigniter-React 3.1.11</title>
+    <title>Codeigniter-React Frontend</title>
 </head>
 <body>
     <div class="jumbotron text-center text-info">
